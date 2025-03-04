604185ec98651eea9e59badbd0f17a4a28005e77
hanzi-code <195461519+hanzi-code@users.noreply.github.com>
2025-03-04 09:39:06 -0300

    Edit scripts.js
    Added new function for the buttons deleted the
    for loop it is not needed anymore with the
    buttons and just added logic to change the texts

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 function playGame() {
     let winnerTxt = document.querySelector(".winner");
+    let roundTxt = document.querySelector(".round-text");
+    let buttons = document.querySelectorAll("button");
 
     let humanScore = 0;
     let computerScore = 0;
-    
+
     // Gets the computer choice with the Math.random() func
     function getComputerChoice() {
         let choice = Math.floor(Math.random() * 3) + 1;
-    
+
         if (choice === 1) {
             return "Paper";
         } else if (choice === 2) {
@@ -16,70 +18,87 @@ function playGame() {
             return "Scissors";
         }
     }
-    
-    // Gets the player choice with a prompt
-    function getHumanChoice() {
-        let choice = prompt("Rock, Paper, Scissors!");
-    
-        return choice;
-    }
+
+    // Gets the player choice, the player can choose betweem 3 buttons
+    buttons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            let target = event.target;
+
+            let getHumanChoice;
+            switch (target.id) {
+                case 'rock':
+                    getHumanChoice = 'Rock';
+                    break;
+                case 'paper':
+                    getHumanChoice = 'Paper';
+                    break;
+                case 'scissors':
+                    getHumanChoice = 'scissors';
+                    break;
+            }
+            let computerChoice = getComputerChoice().toLowerCase();
+            let humanChoice = getHumanChoice.toLowerCase();
+            let result = playRound(humanChoice, computerChoice);
+            humanScore = result.humanScore;
+            computerScore = result.computerScore;
+            winnerTxt.textContent = `Human ${humanScore} - Computer ${computerScore}`;
+
+            // Check if human or machine already won.
+            if (humanScore === 5) {
+                console.log("Human Wins!");
+                winnerTxt.textContent = "Human Wins!";
+                roundTxt.textContent = `Final score is: Human ${humanScore} - Computer ${computerScore}`;
+                humanScore = 0;
+                computerScore = 0;
+            } else if (computerScore === 5) {
+                console.log("Computer Wins!");
+                winnerTxt.textContent = "Computer Wins!";
+                roundTxt.textContent = `Final score is: Computer ${computerScore} - Human ${humanScore}`;
+                humanScore = 0;
+                computerScore = 0;
+            }
+        });
+    });
 
     function playRound(humanChoice, computerChoice) {
-        // Check if the game was a tie and return no points
+        // Check if the round was a tie and return no points
         if (computerChoice === humanChoice) {
-            return {humanScore, computerScore};
+            roundTxt.textContent = `Tie - Human Played ${capitalize(humanChoice)} and Computer Played ${capitalize(computerChoice)}`;
+            return { humanScore, computerScore };
         }
 
         // Check if the computer wins if so return it as a object with
         // one more point
         if (computerChoice === "paper" && humanChoice === "rock") {
-            console.log("You lose! Paper beats Rock");
-            return {humanScore, computerScore: computerScore + 1};
+            roundTxt.textContent = "You lose! Paper beats Rock";
+            return { humanScore, computerScore: computerScore + 1 };
         } else if (computerChoice === "scissors" && humanChoice === "paper") {
-            console.log("You lose! Scissors beats Paper");
-            return {humanScore, computerScore: computerScore + 1};
+            roundTxt.textContent = "You lose! Scissors beats Paper";
+            return { humanScore, computerScore: computerScore + 1 };
         } else if (computerChoice === "rock" && humanChoice === "scissors") {
-            console.log("You lose! Rock beats Scissors");
-            return {humanScore, computerScore: computerScore + 1};
+            roundTxt.textContent = "You lose! Rock beats Scissors";
+            return { humanScore, computerScore: computerScore + 1 };
         }
-    
+
         // Check if the human wins if so return it as a object with
         // one more point
         if (humanChoice === "paper" && computerChoice === "rock") {
-            console.log("You win! Paper beats Rock");
-            return {humanScore: humanScore + 1, computerScore};
+            roundTxt.textContent = "You win! Paper beats Rock";
+            return { humanScore: humanScore + 1, computerScore };
         } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            console.log("You win! Scissors beats Paper");
-            return {humanScore: humanScore + 1, computerScore};
+            roundTxt.textContent = "You win! Scissors beats Paper";
+            return { humanScore: humanScore + 1, computerScore };
         } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            console.log("You win! Rock beats Scissors");
-            return {humanScore: humanScore + 1, computerScore};
+            roundTxt.textContent = "You win! Rock beats Scissors";
+            return { humanScore: humanScore + 1, computerScore };
         }
     }
 
-    // This repeats the game for 5 rounds
-    for (let i = 0; i < 100; i++) {
-        let computerChoice = getComputerChoice().toLowerCase();
-        let humanChoice = getHumanChoice().toLowerCase();
-
-        let result = playRound(humanChoice, computerChoice);
-        humanScore = result.humanScore;
-        computerScore = result.computerScore;
-
-        console.log(`Round ${i + 1}: Human ${humanScore} - Computer ${computerScore}`);
-        
-        // Check if any of the human or machine already have 5 points and then break
-        if (humanScore === 5) {
-            console.log("Human Wins!");
-            winnerTxt.textContent = "Human Wins!";
-            break;
-        } else if (computerScore === 5) {
-            console.log("Computer Wins!");
-            winnerTxt.textContent = "Computer Wins!";
-            break;
-        }
+    // This function is just to capitalize the letters that appear on the canvas of the browser
+    function capitalize(word) {
+        let capitalized = word[0].toUpperCase() + word.slice(1);
+        return capitalized;
     }
-    
 }
 
 playGame();
